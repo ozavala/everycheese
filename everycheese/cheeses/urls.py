1b05c8eb5103e25e6675e3240cba14642c8716ac
Oswaldo Zavala <oszavala@gmail.com>
2020-07-29 17:17:55 -0500

    Implement cheese list page, add navbar link

---
+++ everycheese/cheeses/urls.py
@@ -0,0 +1,13 @@
+# everycheese/cheeses/ursls.py
+from django.urls import path
+from . import views
+
+app_name = "cheeses"
+urlpatterns = [
+    path(
+        route="", 
+        view=views.CheeseListView.as_view(),  
+        name="list"
+    ),    
+]
+
