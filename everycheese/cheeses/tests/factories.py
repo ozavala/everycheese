43690120e2ae02769a9483e812e100b045fe209e
Oswaldo Zavala <oszavala@gmail.com>
2020-07-30 13:31:30 -0500

    Implement cheese country of origin

--- everycheese/cheeses/tests/factories.py
+++ everycheese/cheeses/tests/factories.py
@@ -5,8 +5,10 @@ import factory.fuzzy
 
 from ..models import Cheese
 
+
 class CheeseFactory(factory.django.DjangoModelFactory):
     name = factory.fuzzy.FuzzyText()
+    country_of_origin = factory.Faker('country_code')
     slug = factory.LazyAttribute(lambda obj: slugify(obj.name))
     description = factory.Faker('paragraph', nb_sentences=3, variable_nb_sentences=True)
     firmness = factory.fuzzy.FuzzyChoice(
--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -5,13 +5,10 @@ import pytest
 pytestmark = pytest.mark.django_db
 
 from ..models import Cheese
+from .factories import CheeseFactory
 
 def test___str__():
-    cheese = Cheese.objects.create(
-        name="Stracchino",
-        description="Semi-sweet cheese that goes well with starches",
-        firmness=Cheese.Firmness.SOFT,
-    )
+    cheese = CheeseFactory()
     
-    assert cheese.__str__() == "Stracchino"
-    assert str(cheese) == "Stracchino"
+    assert cheese.__str__() == cheese.name
+    assert str(cheese) == cheese.name
