1277e38a959def5dd6fb7ba8d60aacb44452b51c
Oswaldo Zavala <oszavala@gmail.com>
2020-07-31 15:59:49 -0500

    Modify cheese factory to crete cheese creator user

--- everycheese/cheeses/tests/factories.py
+++ everycheese/cheeses/tests/factories.py
@@ -4,11 +4,12 @@ import factory
 import factory.fuzzy
 
 from ..models import Cheese
-
+from everycheese.users.tests.factories import UserFactory
 
 class CheeseFactory(factory.django.DjangoModelFactory):
     name = factory.fuzzy.FuzzyText()
     country_of_origin = factory.Faker('country_code')
+    creator = factory.SubFactory(UserFactory)
     slug = factory.LazyAttribute(lambda obj: slugify(obj.name))
     description = factory.Faker('paragraph', nb_sentences=3, variable_nb_sentences=True)
     firmness = factory.fuzzy.FuzzyChoice(
