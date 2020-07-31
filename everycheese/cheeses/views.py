5bbdafbed6e7d78e7a628b063a19b46ae9b89471
Oswaldo Zavala <oszavala@gmail.com>
2020-07-31 17:37:32 -0500

    Test the cheese forms and update view

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -2,7 +2,7 @@
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from .models import Cheese
 
@@ -28,3 +28,10 @@ class CheeseCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.creator = self.request.user
         return super().form_valid(form)
+    
+class CheeseUpdateView(LoginRequiredMixin, UpdateView):
+    model = Cheese
+    fields = [
+        'name', 'description', 'firmness', 'country_of_origin',
+    ]
+    action = "update"
